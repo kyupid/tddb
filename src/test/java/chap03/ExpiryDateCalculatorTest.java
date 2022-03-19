257d034bc67c01c466d98c2a9c7aebef42449fbf
kyupid <iamscvv@gmail.com>
2022-03-19 14:04:41 +0900

    구독 서비스 날짜 계산 테스트 시작

---
+++ src/test/java/chap03/ExpiryDateCalculatorTest.java
@@ -0,0 +1,21 @@
+package chap03;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExpiryDateCalculatorTest {
+
+    @Test
+    void 만원_납부하면_한달_뒤가_만료일이_됨() {
+        LocalDate billingDate = LocalDate.of(2022, 3, 19);
+        int payAmount = 10_000;
+
+        ExpiryDateCalculator cal = new ExpiryDateCalculator();
+        LocalDate expiryDate = cal.calculateExpiryDate(billingDate, payAmount);
+
+        assertEquals(LocalDate.of(2022, 4, 19), expiryDate);
+    }
+}
