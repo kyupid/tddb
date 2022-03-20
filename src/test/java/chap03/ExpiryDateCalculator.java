c31c01f2ae713d3c405a770c61211a7864fd2d19
kyupid <iamscvv@gmail.com>
2022-03-20 11:09:29 +0900

    가독성 향상 위한 ExpiryDateCalculator 리팩토링

--- src/test/java/chap03/ExpiryDateCalculator.java
+++ src/test/java/chap03/ExpiryDateCalculator.java
@@ -7,20 +7,26 @@ public class ExpiryDateCalculator {
     public LocalDate calculateExpiryDate(PayData payData) {
         int addedMonths = payData.getPayAmount() / 10_000;
         if (payData.getFirstBillingDate() != null) {
-            LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
-            if(payData.getFirstBillingDate().getDayOfMonth() !=
-            candidateExp.getDayOfMonth()) {
-                if(YearMonth.from(candidateExp).lengthOfMonth() <
-                payData.getFirstBillingDate().getDayOfMonth()) {
-                    return candidateExp.withDayOfMonth(YearMonth.from(candidateExp).lengthOfMonth());
-                }
-                return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth());
-            }
-
-            if (payData.getFirstBillingDate().equals(LocalDate.of(2019, 1, 31))) {
-                return LocalDate.of(2019, 3, 31);
-            }
+            return expiryDateUsingFirstBilingDate(payData, addedMonths);
         }
         return payData.getBillingDate().plusMonths(addedMonths);
+    }
+
+    private LocalDate expiryDateUsingFirstBilingDate(PayData payData, int addedMonths) {
+        LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
+        final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
+        if (dayOfFirstBilling != candidateExp.getDayOfMonth()) {
+            final int dayLenOfCandiMon = YearMonth.from(candidateExp).lengthOfMonth();
+            if (dayLenOfCandiMon < payData.getFirstBillingDate().getDayOfMonth()) {
+                return candidateExp.withDayOfMonth(dayLenOfCandiMon);
+            }
+            return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth());
+        }
+
+        if (payData.getFirstBillingDate().equals(LocalDate.of(2019, 1, 31))) {
+            return LocalDate.of(2019, 3, 31);
+        } else {
+            return candidateExp;
+        }
     }
 }
